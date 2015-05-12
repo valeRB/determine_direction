87f1551c29e860383cbbdd7a4e5016df90beb128
Vale <valerita.rb@gmail.com>
2015-05-12 17:48:25 +0200

    Added the Kalman Filter function on the scripts folder, it is working now for at least tracking when holding the middle, some tuning is required

--- scripts/direction_estimate.py
+++ scripts/direction_estimate.py
@@ -2,22 +2,35 @@
 import roslib
 import rospy
 import math as m
+from matplotlib.pyplot import *
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import WrenchStamped
 from geometry_msgs.msg import Quaternion
 import numpy as np
+import kalman as K
 import tf
 
 
 pub_r = rospy.Publisher("/direction/right_arm", PoseStamped)
+pub_l = rospy.Publisher("/direction/left_arm", PoseStamped)
 
 R_wrench = WrenchStamped()
 L_wrench = WrenchStamped()
 R_dir = PoseStamped()
 L_dir = PoseStamped()
-#quat = Quaternion()
+
+xhat_0 = np.matrix('1.57 ; 0.0') # start estimate at pi/2 for theta
+P_0 = np.matrix(np.identity(2))
+first_estimate = 1
+THETA = np.array([])
+THETA_2q = np.array([])
+THETA_hat = np.array([])
+mag_FORCE = np.array([])
+xhat = np.matrix('1.57 ; 0.0')
+P = np.matrix([])
 
 def r_direction_callback(msg):
+    global THETA, THETA_hat, first_estimate, xhat, P, THETA_2q, mag_FORCE
     R_wrench = msg
     br_r = tf.TransformBroadcaster()
     br_r.sendTransform((0, 0, 0),
@@ -27,30 +40,70 @@ def r_direction_callback(msg):
          "r_gripper_motor_accelerometer_link")
     N_force = m.sqrt((m.pow(R_wrench.wrench.force.x, 2) +
                m.pow(R_wrench.wrench.force.z, 2)))
+    mag_FORCE = np.append(mag_FORCE, N_force)
 ##    theta = m.acos(R_wrench.wrench.force.x / N_force)
-    theta = m.atan2(R_wrench.wrench.force.z, R_wrench.wrench.force.x)
+    theta_r = m.atan2(R_wrench.wrench.force.z, R_wrench.wrench.force.x)
+    if theta_r < 0:
+        theta_r_2q = theta_r + m.pi
+    else:
+        theta_r_2q = theta_r
+    THETA = np.append(THETA, theta_r)
+    THETA_2q = np.append(THETA_2q, theta_r_2q)
+    if first_estimate == 1:
+        (xhat, P) = K.Kalman_Filter(theta_r_2q, xhat_0, P_0)
+        THETA_hat = np.append(THETA_hat, xhat[0])
+        first_estimate = 0
+        print ("xhat is: "  , xhat[0])
+        
+    else:
+        (xhat, P) = K.Kalman_Filter(theta_r_2q, xhat, P)
+        THETA_hat = np.append(THETA_hat, xhat[0])
+        
+    
     ## -- 0 deg= 0 rad; 90 = pi/2; 180 = pi; 270 = -pi/2
-    rospy.loginfo("Theta: %f", theta)
+##    if theta_r > 0: #transform to visualize direction towards +Z
+##        theta_r += m.pi
+##    rospy.loginfo("Theta: %f", theta_r)
 ##    print("45 deg", m.atan2(1,-1))
 ##    print("225 deg", m.atan2(-1,1))
 ##    rospy.loginfo("Fz: %f", R_wrench.wrench.force.z)
 ##    rospy.loginfo("Check N_force: %f", N_force)
-##    rospy.loginfo("Check theta: %f", theta)
-    R_dir.header.frame_id = 'ft_transform_r'
-    quat = tf.transformations.quaternion_from_euler(0.0, theta, 0.0)
-      
-    R_dir.pose.orientation.x = quat[0]
-    R_dir.pose.orientation.y = quat[1]
-    R_dir.pose.orientation.z = quat[2]
-    R_dir.pose.orientation.w = quat[3]
-    
-    pub_r.publish(R_dir)    
+##    rospy.loginfo("Check theta: %f", theta_r)
+##    R_dir.header.frame_id = 'ft_transform_r'
+##    quat = tf.transformations.quaternion_from_euler(0.0, theta_r, 0.0)
+##      
+##    R_dir.pose.orientation.x = quat[0]
+##    R_dir.pose.orientation.y = quat[1]
+##    R_dir.pose.orientation.z = quat[2]
+##    R_dir.pose.orientation.w = quat[3]
+##    
+##    pub_r.publish(R_dir)    
     
     return
 
 def l_direction_callback(msg):
-##    L_wrench = msg
-##    R_pose.header.frame_id = 'l_force_torque_adapter_link'
+    L_wrench = msg
+    br_l = tf.TransformBroadcaster()
+    br_l.sendTransform((0, 0, 0),
+         tf.transformations.quaternion_from_euler(0.0, 1.57, 0.0),
+         rospy.Time.now(),
+         "ft_transform_l",
+         "l_gripper_motor_accelerometer_link")
+    theta_l = m.atan2(L_wrench.wrench.force.z, L_wrench.wrench.force.x)
+    ## -- 0 deg= 0 rad; 90 = pi/2; 180 = pi; 270 = -pi/2
+##    if theta_l > 0:
+##        theta_r += m.pi
+##    rospy.loginfo("Theta: %f", theta_r)
+    L_dir.header.frame_id = 'ft_transform_l'
+    quat_l = tf.transformations.quaternion_from_euler(0.0, theta_l, 0.0)
+      
+    L_dir.pose.orientation.x = quat_l[0]
+    L_dir.pose.orientation.y = quat_l[1]
+    L_dir.pose.orientation.z = quat_l[2]
+    L_dir.pose.orientation.w = quat_l[3]
+    
+    pub_l.publish(L_dir)    
+    
     return
 
 
@@ -59,9 +112,8 @@ def l_direction_callback(msg):
 def direction_estimate():
     rospy.init_node('direction_estimate', anonymous=True)
     rospy.Subscriber("/ft_transformed/rig_arm", WrenchStamped, r_direction_callback)
-    rospy.Subscriber("/ft_transformed/lef_arm", WrenchStamped, l_direction_callback)
+    #rospy.Subscriber("/ft_transformed/lef_arm", WrenchStamped, l_direction_callback)
     
-    br_l = tf.TransformBroadcaster()
     rospy.spin()
     return
 
@@ -74,13 +126,34 @@ if __name__ == '__main__':
         rate = rospy.Rate(10.0)
         while not rospy.is_shutdown():
 
-            br_l.sendTransform((0, 0, 0),
-                     tf.transformations.quaternion_from_euler(1.57, -3.14, 0.0),
-                     rospy.Time.now(),
-                     "ft_transform_l",
-                     "l_gripper_motor_accelerometer_link")
             rate.sleep()
 
     except KeyboardInterrupt:
         pass
+    raw_input("Press any key to see plot of theta")
+    X_axis = np.linspace(0,(len(THETA)-1),len(THETA))
+    print('len(THETA)', len(THETA))
+##    print('len(THETA_hat)', len(THETA_hat))
+    print('len(THETA_2q)', len(THETA_2q))
+    print('len(X_axis)',len(X_axis))
+    figure("Theta and theta_hat")
+    subplot(411)
+    plot(X_axis, THETA, 'g'), title('THETA'), ylabel('[rad]')
+    subplot(412)
+    plot(X_axis, THETA_2q, 'b'), title('THETA_2q')
+    plot(X_axis, THETA_hat, 'r'), title('THETA_hat')
+    legend(('Theta_2q','THETA_hat'),'upper right')
+    subplot(413)
+    plot(X_axis, (THETA_2q - THETA_hat), 'r'), title("Error")
+    
+##    legend(('Theta','Theta_hat'),'upper right')
+##
+    subplot(414)
+    plot(X_axis, mag_FORCE), title('magnitude of force' )
+    #subplot(212)
+    #plot(X_axis, (THETA - THETA_2q), 'r'), title("Error")
+    
+    
+    
+    show()
 
