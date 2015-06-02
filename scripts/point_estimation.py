82c2e7161485dbd76807db13d701703124e8da33
Vale <valerita.rb@gmail.com>
2015-06-02 16:34:18 +0200

    Added plots for error and modified KFs parameters

--- scripts/point_estimation.py
+++ scripts/point_estimation.py
@@ -3,31 +3,45 @@ import roslib
 import rospy
 from matplotlib.pyplot import *
 from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import PointStamped
 import numpy as np
 import math as m
 import tf
 
 #first_time = True
-save_array = True
+save_array = False
 wrench = WrenchStamped()
+R_vect = PointStamped()
+true_point = PointStamped()
+pub_R = rospy.Publisher("/point_est/estimation", PointStamped)
+pub_t = rospy.Publisher("/point_est/truth", PointStamped)
+
+# --- True point ---
+true_point.point.x = 0
+true_point.point.y = 0
+true_point.point.z = 0.64
 
 # --- Kalman Filter initialization ---
 wStd = 0.01
-vStd = 500
-A = np.eye(3)
+wStd1 = 0.01
+vStd = 5
+A = np.eye(6)
 A = np.mat(A)
-I = np.eye(3)
+I = np.eye(6)
 I = np.mat(I)
-r_hat_k = np.matrix([[0], [0], [0]])
-P_k = np.eye(3)
+r_hat_k = np.matrix([[0], [0], [0], [0], [0], [0]])
+P_k = np.eye(6)
 P_k = np.mat(P_k)
-Q = np.eye(3)*(m.pow(wStd,2))
+Q = np.eye(6)* (m.pow(wStd,2))
 Q = np.mat(Q)
-R = np.eye(3)*(m.pow(vStd,2))
+Q[3,3] = m.pow(wStd1,2)
+Q[4,4] = m.pow(wStd1,2)
+Q[5,5] = m.pow(wStd1,2)
+R = np.eye(6)* (m.pow(vStd,2))
 R = np.mat(R)
-H = np.zeros((3,3))
+H = np.zeros((6,6))
 H = np.mat(H)
-z_k = np.zeros((3,1))
+z_k = np.zeros((6,1))
 
 # Array vectors
 r_hat_A = np.empty([3,0])
@@ -43,19 +57,37 @@ def wrench_callback(msg):
 	global wrench, Torque, H
 	global r_hat_A, Fx, Fy, Fz, Tx, Ty, Tz
 	wrench = msg
-	H[0,1] = wrench.wrench.force.z
-	H[0,2] = -wrench.wrench.force.y
-	H[1,0] = -wrench.wrench.force.z
-	H[1,2] = wrench.wrench.force.x
-	H[2,0] = wrench.wrench.force.y
-	H[2,1] = -wrench.wrench.force.x
+	H[0,4] = wrench.wrench.force.z
+	H[0,5] = -wrench.wrench.force.y
+	H[1,3] = -wrench.wrench.force.z
+	H[1,5] = wrench.wrench.force.x
+	H[2,3] = wrench.wrench.force.y
+	H[2,4] = -wrench.wrench.force.x
 	z_k[0] = wrench.wrench.torque.x
 	z_k[1] = wrench.wrench.torque.y
 	z_k[2] = wrench.wrench.torque.z
 
 	r_hat_k = Kalman_filter(H, z_k)
+
+	# --- Create new frame for publishing theta -----
+	br_r = tf.TransformBroadcaster()
+	br_r.sendTransform((0, 0, 0), 
+		tf.transformations.quaternion_from_euler(0.0, 1.57, 0.0),
+		rospy.Time.now(),
+		"ft_transform_r",
+		"r_gripper_motor_accelerometer_link")
+	# ----------------------------------------------
+	R_vect.header.frame_id = 'ft_transform_r'
+	R_vect.point.x = r_hat_k[3]
+	R_vect.point.y = r_hat_k[4]
+	R_vect.point.z = r_hat_k[5]
+	pub_R.publish(R_vect)
+
+	true_point.header.frame_id = 'ft_transform_r'
+	pub_t.publish(true_point)
+	
 	if save_array == True:
-		r_hat_A = np.hstack((r_hat_A, r_hat_k))
+		r_hat_A = np.hstack((r_hat_A, r_hat_k[3:]))
 		Fx = np.append(Fx, wrench.wrench.force.x)
         Fy = np.append(Fy, wrench.wrench.force.y)
         Fz = np.append(Fz, wrench.wrench.force.z)
@@ -67,33 +99,21 @@ def wrench_callback(msg):
 def Kalman_filter(H, z_k):
 	global A, r_hat_k, P_k, Q, R, I
 	z_k = np.mat(z_k)
+	# print ('H', H)
+	# print ('z_k', z_k)
+	# print ('Q', Q)
+	# print ('R', R)
 	# -------- Prediction Step --------
-	r_hat_k = A * r_hat_k # [3x1]
+	r_hat_k = A * r_hat_k # [6x1]
 	#print ('Pred r_hat_k', r_hat_k)
-	# print ('type:', type(r_hat_k))
 	P_k = P_k + Q # [3x3]; A * P_k * A' + Q; A = A' in this case
 	#print ('Pred P_K', P_k)
-	# print ('type(P_K)', type(P_k))
-	#print ('H', H)
-	# print ('type(H)', type(H))
 	# -------- Update Step -----------
-	# print('Breaking down Kalman gain')
-	# test1 = (H * P_k * (-H)) # Correct result
-	# test2 = (H * P_k * (-H)) + R #Correct result
-	# test3 = np.linalg.inv( (H * P_k * (-H)) + R )
-	# test4 = P_k * (-H) # Correct
-	# test5 = P_k * (-H) * np.linalg.inv( (H * P_k * (-H)) + R )
-	# print('test1: ', test1)
-	# print('test2: ', test2)
-	# print('test3: ', test3)
-	# print('test4: ', test4)
-	# print('test5: ', test5)
-	# print('type test1', type(test1))
-	# print('type test2', type(test2))
-	# print('type test3', type(test3))
-	# print('type test4', type(test4))
-	# print('type test5', type(test5))
-	K_k = P_k * (-H) * np.linalg.inv( (H * P_k * (-H)) + R ) # [3x3]; PH'(HPH'+R)-1; H'=-H
+	K_k = P_k * (H.transpose()) * np.linalg.inv( (H * P_k * (H.transpose()) ) + R ) # [3x3]; PH'(HPH'+R)-1; H'=-H
+	# print('K_k', K_k)
+	# print('H * r_hat_k', H * r_hat_k)
+	# print('z_k - H * r_hat_k', z_k - H * r_hat_k)
+	# print('K_k * ( z_k - H * r_hat_k )', K_k * ( z_k - H * r_hat_k ))
 	r_hat_k = r_hat_k + K_k * ( z_k - H * r_hat_k ) # [3x1]
 	#print("r_hat_k", r_hat_k)
 	P_k = (I - K_k * H) * P_k # [3x3]
@@ -102,10 +122,9 @@ def Kalman_filter(H, z_k):
 
 def point_estimation():
 	rospy.init_node('point_estimation', anonymous=True)
-	rospy.Subscriber("/ft_transformed/lef_arm", WrenchStamped, wrench_callback)
+	rospy.Subscriber("/ft_transformed/rig_arm", WrenchStamped, wrench_callback)
 	rospy.spin()
 	return
-
 if __name__ == '__main__':
 
 	point_estimation()
@@ -117,15 +136,15 @@ if __name__ == '__main__':
 
 	except KeyboardInterrupt:
 		pass
-	raw_input("\n Press key to continue")
+	
 	if save_array == True:
+		raw_input("\n Press key to continue")
 		r_x = np.array(r_hat_A[0,:])
 		r_y = np.array(r_hat_A[1,:])
 		r_z = np.array(r_hat_A[2,:])
 
 		print('size(r_x)', np.size(r_x, 1))
 		X_axis = np.linspace(0,(np.size(r_x, 1)-1),np.size(r_x, 1))
-		X_for_r = np.array([X_axis])
 		print('len(X_axis)',len(X_axis))
 		print("len(Fx)",len(Fx))
 
@@ -146,13 +165,53 @@ if __name__ == '__main__':
 		subplot(313)
 		plot(X_axis, Tz, 'b'), title('Tz'), ylabel('[Nm]')
 
+		# ---- Ground truth creation ----
+		show_gnd_truth = True
+		start_pt = 0 #13100
+		X_value = 0 # [m]
+		Z_value = 0.64 # [m]
+		gnd_X = np.zeros(len(X_axis))
+		gnd_Y = np.zeros(len(X_axis))
+		gnd_Z = np.zeros(len(X_axis))
+		gnd_X[start_pt:] = X_value
+		gnd_Z[start_pt:] = Z_value
+		ymax = 0.9
+		ymin = -0.2
 
+		
+
+		#print('type(r_y-gnd_X)',type(r_y-gnd_X))
+		#print(r_y - gnd_X)
 		figure(3)
-		subplot(311)
+		# ---- R_x ----
+		subplot(321), ylim((ymin, ymax)) #, subplots_adjust(left=0.08,bottom=0.06,right=0.93,top=0.92, wspace=0.20, hspace = 0.33)
 		plot(X_axis , np.squeeze(r_x), 'r'), title('Estimate of R_x'), ylabel('[m]')
-		subplot(312)
+		if show_gnd_truth == True:
+			plot(X_axis, gnd_X, '--k')
+			legend(('X_hat' , 'True X'), 'lower right')
+			subplot(322), ylim((ymin, ymax))
+			plot(X_axis, np.squeeze(abs(r_x - gnd_X)), 'r'), title('Estimation error for R_x')
+			plot(X_axis, gnd_Y, '--k')
+		
+		# ---- R_y ----
+		subplot(323), ylim((ymin, ymax))
 		plot(X_axis, np.squeeze(r_y), 'g'), title('Estimate of R_y'), ylabel('[m]')
-		subplot(313)
+		if show_gnd_truth == True:
+			plot(X_axis, gnd_Y, '--k')
+			legend(('Y_hat' , 'True Y'), 'lower right')
+			subplot(324), ylim((ymin, ymax))
+			plot(X_axis, np.squeeze(abs(r_y - gnd_Y)), 'g'), title('Estimation error for R_y')
+			plot(X_axis, gnd_Y, '--k')
+		
+		# ---- R_y ----
+		subplot(325), ylim((ymin, ymax))
 		plot(X_axis, np.squeeze(r_z), 'b'), title('Estimate of R_z'), ylabel('[m]')
+		if show_gnd_truth == True:
+			plot(X_axis, gnd_Z, '--k')
+			legend( ('Z_hat' , 'True Z'), 'lower right')
+			subplot(326), ylim((ymin, ymax))
+			plot(X_axis, np.squeeze(abs(r_z - gnd_Z)), 'b'), title('Estimation error for R_z')
+			plot(X_axis, gnd_Y, '--k')
+
 
 		show()
